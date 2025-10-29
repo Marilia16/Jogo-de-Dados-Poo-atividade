62467cc8de67845faa2852a23454f8abc76f2699
Marília Gabriele <161947955+Marilia16@users.noreply.github.com>
2025-10-29 09:18:33 -0300

    Add files via upload

---
+++ JogodeDados/src/main/java/com/marilia16/jogodedados/JogodeDadosApplication.java
@@ -0,0 +1,13 @@
+package com.marilia16.jogodedados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogodeDadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogodeDadosApplication.class, args);
+	}
+
+}
---
+++ JogodeDados/src/test/java/com/marilia16/jogodedados/JogodeDadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marilia16.jogodedados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JogodeDadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
